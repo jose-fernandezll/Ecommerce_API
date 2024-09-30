d5b4373a4802c531c83355925f2d0d839782c63c
jose-fernandezll <jose.ismael.fernandez001@gmail.com>
2024-09-29 21:27:27 -0500

    added some test for products

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -8,17 +8,39 @@ RSpec.describe "Carts", type: :request do
     let!(:product_2) { create(:product) }
 
     describe '/products' do
-      it '' do
+      it 'should have a http status code :ok' do
+        get products_url
+        expect(response).to be_successful
+      end
+
+      it 'should be return 2 products' do
+        get products_url
+        expect(json_body.count).to eq(2)
+      end
+
+      it 'should be match product 1 and 2' do
+        products = []
+        products.append(JSON.parse(product_1.to_json))
+        products.append(JSON.parse(product_2.to_json))
+
         get products_url
 
-        binding.irb
-        expect(response).to be_successful
+        expect(json_body).to eq(products)
       end
     end
 
-    describe '/products/' do
-      it '' do
+    describe '/products/:id' do
+      it 'should have a http status code :ok' do
+        get product_url(product_1)
 
+        expect(response).to be_successful
+      end
+
+      it 'should be match product 1' do
+        product = JSON.parse(product_1.to_json)
+        get product_url(product_1)
+
+        expect(json_body).to eq(product)
       end
     end
   end
