ecfe8bcfa7c7c09ea83133dabf3833bd03662240
jose-fernandezll <jose.ismael.fernandez001@gmail.com>
2024-09-29 21:36:00 -0500

    improve some test for cart

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -3,37 +3,47 @@ require 'rails_helper'
 RSpec.describe "Carts", type: :request do
   include_context :login_user
 
-  describe "GET" do
-    it '/carts' do
-      get cart_url
-      expect(response).to be_successful
+  context 'GET' do
+    describe "/cart" do
+      it 'should have a http status code :ok' do
+        get cart_url
+        expect(response).to be_successful
+      end
     end
   end
 
-  describe "POST" do
-    let(:product) { create(:product) }
+  context 'POST' do
+    describe "/cart/add_item" do
+      let(:product) { create(:product) }
 
-    it '/cart/add_item' do
-      post add_item_cart_url, params: { product_id: product.id }
-
-      expect(response).to be_successful
+      it 'should have a http status code :ok' do
+        post add_item_cart_url, params: { product_id: product.id }
+        expect(response).to be_successful
+      end
+      # deberia haber mas test para comprobar que si se agrega el producto
     end
   end
 
-  describe "DELETE" do
-    let(:product) { create(:product) }
-    it 'cart/remove_item/' do
-      user.cart.products.append(product)
+  context 'DELETE' do
+    describe "cart/remove_item/:id" do
+      let(:product) { create(:product) }
 
-      delete remove_item_cart_url, params: { product_id: product.id }
+      it 'should have a http status code :ok' do
+        user.cart.products.append(product)
 
-      expect(response).to be_successful
-    end
+        delete remove_item_cart_url, params: { product_id: product.id }
 
-    it 'cart/remove_item/' do
-      delete remove_item_cart_url, params: { product_id: product.id }
+        expect(response).to be_successful
+      end
 
-      expect(json_body['error']).to eq("Item not found in cart")
+      it 'should not found the item in the car' do
+        delete remove_item_cart_url, params: { product_id: product.id }
+
+        expect(json_body['error']).to eq("Item not found in cart")
+      end
+
+      # deberia haber mas test para comprobar que si se elimina el producto
+
     end
   end
 end
