12632b5718255cbff6e8db17066b407818e7d6b2
jose-fernandezll <jose.ismael.fernandez001@gmail.com>
2024-10-12 23:54:11 -0500

    improve the products controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,6 +40,41 @@ class OrdersController < ApplicationController
     render json: OrderSerializer.new(@order, include: [:order_items]).serializable_hash.to_json
   end
 
+  def update
+    ActiveRecord::Base.transaction do
+      update_order_items
+      @order.save!
+    end
+
+    render json: { order: serialized_body('OrderSerializer', @order) }, status: :ok
+  end
+
+  private
+
+  def set_order
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def update_order_items
+
+    params[:order][:order_items].each do |item_params|
+      product = Product.find(item_params[:product_id])
+      order_item = @order.order_items.find_or_initialize_by(product: product)
+
+      if item_params[:quantity].to_i <= 0
+        order_item.destroy
+      else
+
+        order_item.update!(quantity: item_params[:quantity])
+      end
+
+      if product.stock < order_item.quantity
+        raise ActiveRecord::RecordInvalid.new(@order), "Insufficient stock for product #{product.name}"
+      end
+    end
+  end
+
+
   private
 
   def set_cart
@@ -48,5 +83,9 @@ class OrdersController < ApplicationController
 
   def find_order
     @order = current_user.orders.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(order_items: [:product_id, :quantity])
   end
 end
